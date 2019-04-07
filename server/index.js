287e015b81cb3e3bff16ebe9e008f90c0b18549e
Samuel Robinson <pinkstarsr11@gmail.com>
2019-04-06 22:13:34 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,152 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const path = require("path");
+
+let cacheTimeout = null;
+const messageCacheTimeout = 20000;
+const serverPort = 80;
+
+const maxNameLength = "Kyndrajauna".length;
+const maxTextLength = 256;
+
+const validCharacters = " abcdefghjijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()-=_+[]{};:'\",./?<>\\";
+const validCharacterLookup = {};
+validCharacters.split("").forEach(character => validCharacterLookup[character] = true);
+
+const messageBuffer = [];
+const cacheMessages = function() {
+    let fileData = "";
+    messageBuffer.forEach(message => {
+        fileData += JSON.stringify(message);
+        fileData += "\n";
+    });
+    fs.writeFile("messages.txt",fileData,() => {
+        console.log("Saved message cache to disk (probably)");
+    });
+}
+const loadOldMessages = function(callback) {
+    fs.readFile("messages.txt",(error,data) => {
+        if(error) {
+            console.log(error);
+            return;
+        }
+        data = data.toString();
+        if(!data) {
+            console.log("Message cache was empty, loaded nothing into the cache");
+            callback();
+            return;
+        }
+        const lines = data.toString().split("\n");
+        if(!lines.length) {
+            console.log("Message cache was empty, loaded nothing into the cache");
+            callback();
+            return;
+        }
+        lines.forEach(line => {
+            let message;
+            let hadError = false;
+            try {
+                message = JSON.parse(line);
+            } catch(exception) {
+                console.log(exception);
+                hadError = true;
+            }
+            if(!hadError) {
+                const processedMessage = processMessage(message);
+                if(processedMessage) {
+                    messageBuffer.push(processedMessage);
+                } else {
+                    console.log("There was an invalid message in the message cache, skipping this one");
+                }
+            } else {
+                console.log("JSON parsing error for a cached message, skipping this one");
+            }
+        });
+        callback();
+    });
+}
+const containsValidCharacters = function(text) {
+    for(let character of text) {
+        if(!validCharacterLookup[character]) {
+            return false;
+        }
+    }
+    return true;
+}
+const nameIsTooLong = function(name) {
+    return name.length > maxNameLength;
+}
+const textIsTooLong = function(text) {
+    return text.length > maxTextLength;
+}
+const processMessage = function(message) {
+    let messageData = typeof message === "object" ? message : null;
+    try {
+        if(!messageData) {
+            messageData = JSON.parse(message);
+        }
+    } catch(exception) {
+        return false;
+    }
+    if(Object.keys(messageData).length !== 2) {
+        return false;
+    }
+    if(messageData.name && (messageData.name = messageData.name.trim()).length > 0) {
+        if(nameIsTooLong(messageData.name)) {
+            return false;
+        }
+        if(!containsValidCharacters(messageData.name)) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    if(messageData.text && (messageData.text = messageData.text.trim()).length > 0) {
+        if(textIsTooLong(messageData.text)) {
+            return false;
+        }
+        if(!containsValidCharacters(messageData.text)) {
+            return false;
+        }
+        return messageData;
+    } else {
+        return false;
+    }
+}
+
+app.use("/",express.static(path.join(__dirname,"../web/")));
+
+app.get("/", function(_,response) {
+    response.sendFile("index.html");
+});
+const namespace = io.of("hello-world");
+const sendMessageToClients = function(message) {
+    namespace.emit("incoming-message",JSON.stringify(message));
+    messageBuffer.unshift(message);
+    clearTimeout(cacheTimeout);
+    cacheTimeout = setTimeout(cacheMessages,messageCacheTimeout);
+}
+const incomingMessage = function(message) {
+    const processedMessage = processMessage(message);
+    if(processedMessage) {
+        console.log(processedMessage);
+        sendMessageToClients(processedMessage);
+    } else {
+        console.log("Received an invalid message");
+    }
+}
+const handleConnection = function(socket) {
+    if(socket.handshake && socket.handshake.address) {
+        console.log(socket.handshake.address);
+    }
+    socket.emit("receive-old-messages",JSON.stringify({
+        messages: messageBuffer
+    }));
+    socket.on("world-message",incomingMessage);
+}
+namespace.on("connection",handleConnection);
+
+loadOldMessages(() => {server.listen(serverPort)});
---
+++ web/main.js
@@ -0,0 +1,115 @@
+const sendButton = document.getElementById("send-button");
+const textInput = document.getElementById("text-input");
+const messageContainer = document.getElementById("messages");
+
+let name = localStorage.getItem("name") || "Bob";
+localStorage.setItem("name",name);
+const validateName = function(name) {
+    if(name.length > maxNameLength) {
+        alert("Your name is too long");
+        return false;
+    }
+    for(let character of name) {
+        if(!validCharacterLookup[character]) {
+            alert(`Names cannot contain the character '${character}'`);
+            return false;
+        }
+    }
+    name = name.trim();
+    if(!name) {
+        alert("You cannot not have a name");
+        return false;
+    }
+    return name;
+}
+const getMyName = function() {
+    return name;
+}
+const changeName = function() {
+    const newName = prompt("What do you want your new name to be?",name);
+    if(!newName) {
+        return;
+    }
+    const validatedName = validateName(newName);
+    if(validatedName) {
+        name = validatedName;
+        localStorage.setItem("name",validatedName);
+    }
+}
+const addMessageToInterface = function(message) {
+    const messageElement = document.createElement("div");
+    messageElement.className = "message";
+    const titleElement = document.createElement("p");
+    titleElement.className = "message-title";
+    const textElement = document.createElement("p");
+    textElement.className = "message-text";
+    titleElement.appendChild(document.createTextNode(
+        `message from ${message.name}`
+    ));
+    textElement.appendChild(document.createTextNode(
+        message.text
+    ));
+    messageElement.appendChild(titleElement);
+    messageElement.appendChild(textElement);
+    messageContainer.prepend(messageElement);
+}
+const socket = io.connect("http://localhost/hello-world");
+
+socket.on("incoming-message", function(data) {
+    const message = JSON.parse(data);
+    addMessageToInterface(message);
+});
+socket.on("receive-old-messages", function(data) {
+    while(messageContainer.lastChild) {
+        messageContainer.removeChild(messageContainer.lastChild);
+    }
+    const messages = JSON.parse(data).messages;
+    messages.reverse().forEach(addMessageToInterface);
+});
+const sendMessageToServer = function(message) {
+    socket.emit("world-message",JSON.stringify(message));
+}
+
+const maxNameLength = "Kyndrajauna".length;
+const maxTextLength = 256;
+const validCharacters = " abcdefghjijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()-=_+[]{};:'\",./?<>\\";
+const validCharacterLookup = {};
+validCharacters.split("").forEach(character => validCharacterLookup[character] = true);
+const textLengthErrorMessage = `Text length cannot be greater than ${maxNameLength} character${maxNameLength===1?"":"s"}`;
+
+const sendMessage = function() {
+    const text = textInput.value.trim();
+    if(text) {
+        if(text.length > maxTextLength) {
+            alert(textLengthErrorMessage);
+            return;
+        }
+        for(let character of text) {
+            if(!validCharacterLookup[character]) {
+                alert(`Text cannot contain the character '${character}'`);
+                return;
+            }
+        }
+        const name = getMyName();
+        if(!name) {
+            alert("You must have a name to send a message");
+            return;
+        }
+        textInput.value = "";
+        sendMessageToServer({
+            name: name,
+            text: text
+        });
+    } else {
+        alert("Empty text cannot be sent");
+    }
+}
+
+sendButton.onclick = sendMessage;
+
+textInput.onkeydown = function(event) {
+    if(event.key === "Enter") {
+        sendMessage();
+        event.preventDefault();
+    }
+}
